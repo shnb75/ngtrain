5487079be501072091bde4db36d81b4b73e61cb2
shnb <nikbafshahab@gmail.com>
2021-11-14 04:59:09 -0800

    for train

--- src/main.ts
+++ src/main.ts
@@ -10,39 +10,20 @@ if (environment.production) {
 
 
 console.log("---------------------------------------");
+let cars:string[]=["pride","206","207"];
+for (let i=0; i<cars.length; i++){
+  console.log(cars[i]);
 
-let a:number=3;
-console.log(a);
-let b:string="sdts";
-console.log(b);
-let s:boolean=true;
-console.log(s);
-let cars:string [] =["pride","206","pars"];
-console.log(cars);
-let num:number [] =[2,3,5,6,9];
-console.log(num);
-let f:any = "dfghfsd";
-f=[2,3,5,6];
-console.log(f);
-let g:any[]=["salal",2,false];
-console.log(g);
-let h:number|string="sgdsg";
-// h=2;
-console.log(h);
-console.log(typeof h);
-if (typeof h == "number"){
-  console.log("h is number");
-}else if(typeof h == "string") {
-console.log("h is string");
 }
-let j :"ali"|2|'reza'="reza";
-console.log(j);
-let bb;
-console.log(bb);
 
 
+for(let item of cars){
+  console.log(item);
+}
 
-
+for (let idx in cars){
+  console.log(cars[idx]);
+}
 
 console.log("---------------------------------------");
 
